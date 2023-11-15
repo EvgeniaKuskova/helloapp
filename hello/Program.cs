8ab4ee2011de613b43376ffa4e2b26051d4d95dd
black <black@example.com>
2023-11-16 02:01:55 +0300

    hello everyone

--- hello/Program.cs
+++ hello/Program.cs
@@ -4,7 +4,7 @@
   {
     public static void Main(string[] args)
     {
-      System.Console.WriteLine("hello");
+      System.Console.WriteLine("hello everyone");
     }
 
     public static void GreetWhite()
